6ee658c861441ab5cf3fb6b879db2942673ec6ed
Tirta Styadi <tirta.setiady@yahoo.com>
2022-02-11 14:15:10 +1100

    Update function to generate file

--- index.js
+++ index.js
@@ -6,74 +6,129 @@ const fs = require('fs');
 // Array of questions for user input
 const promtUser = () => {
     return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-          },
-          {
-            type: 'input',
-            name: 'location',
-            message: 'Where are you from?',
-          },
-          {
-            type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
-          },
-          {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-          },
-          {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-          },
-          {
-            type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
-          },
+      {
+        type: 'input',
+        message:'What is name of the project?', 
+        name: 'title',
+    },
+    {
+      type: 'input',
+      message:'Enter your fullname?', 
+      name: 'name',
+    },
+    {
+        type: 'list',
+        message: 'What licence are you working with?',
+        name: 'license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'Boost 1.0', 'Unlicense'],
+    },
+    {
+        type: 'input',
+        message:'Motivation to build this project?', 
+        name: 'motivation',
+    },
+    {
+        type: 'input',
+        message:'Write the FIRST step on how to use it', 
+        name: 'step1',
+    },
+    {
+        type: 'input',
+        message:'Write the SECOND step on how to use it ', 
+        name: 'step2',
+    },
+    {
+        type: 'input',
+        message:'Write the THIRD step on how to use it ', 
+        name: 'step3',
+    },
+    {
+        type: 'input',
+        message:'Write the FOURTH step on how to use it ', 
+        name: 'step4',
+    },
+    {
+        type: 'input',
+        message: 'Input your resource link using this format, https:// ', 
+        name: 'resourceLink',
+    },
+    {
+      type: 'input',
+      message: 'Input website link using this format, https:// ', 
+      name: 'webpageLink',
+    },
+    {
+        type: 'input',
+        message: 'Input your email:', 
+        name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Input year of production for license:', 
+      name: 'year',
+    }
     ])
-}
+};
 
 // Function to write README file - structure of the html
-const writeToFile = ({ name, location, github, linkedin}) => 
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+const writeToFile = ({ title, name,license, motivation, step1, step2, step3, step4, webpageLink, resourceLink, email, year}) => 
+`# ${title}
+## ${name} 
+---
+### License
+
+---
+
+
+## Contents:
+- [Motivation](#motivation)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributor](#contribution)
+
+---
+
+## Motivation 
+// ### Motivation behind the project?
+${motivation}
+
+---
+
+## Installation:
+- ${step1}
+- ${step2}
+- ${step3}
+- ${step4}
+
+---
+
+Webpage link and resource are provided to further check and test.
+
+* Webpage Link
+https://${webpageLink}
+
+* Resource Link
+https://${resourceLink}
+
+---
+
+
+## Contributor(s)
+
+- ${name} - ${email}
+
+---
+
+## © ${name}, ${year}`;
 
 // Initialize app function to generate readme file
 const init = () => {
     promtUser()
       // Use writeFileSync method to use promises instead of a callback function
-      .then((answers) => fs.writeFileSync('Readme.md', writeToFile(answers)))
+      .then((answers) => fs.writeFileSync('README.md', writeToFile(answers)))
       .then(() => console.log('Successfully wrote Readme File'))
       .catch((err) => console.error(err));
   };
-console.log(inquirer.name);
-
 
 // Function call to initialize app
 init();
